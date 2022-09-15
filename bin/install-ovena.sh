7194c27b90399b43562d3402d6cd53602864b00b
Toni Magni <email@antoniomagni.com>
2022-09-15 17:26:36 -0400

    Fix orthanc config in wrong place.

--- bin/install-ovena.sh
+++ bin/install-ovena.sh
@@ -7,7 +7,8 @@ mkdir -vp "$<ORTHANC_CONFIG>" "$<ORTHANC_DATA_MOUNT>" "$<POSTGRESQL_DATA>" || ex
 mkdir -vp "$(dirname $<POSTGRESQL_DB_DUMP>)" || exit 1
 
 echo "Update and install packages"
-apt update && apt upgrade && apt install docker.io docker-compose || exit 1
+apt update && apt-get -y upgrade && apt install docker.io docker-compose || exit 1
+apt autoremove
 
 echo "Copy configuration files"
 cp -vR docker/* "$<ORTHANC_CONFIG>" || exit 1
