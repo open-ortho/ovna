f55619694b8047ea945db400e41a7169f37b2b0f
Toni Magni <email@antoniomagni.com>
2022-11-03 19:18:19 +0100

    Remove keys, remove Dockerfiles

--- bin/ovena-install.sh
+++ bin/ovena-install.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-echo "This is Ovena Installer"
+printf "
+***********************
+This is Ovena Installer
+***********************
+
+"
 
 # If ovena is running, shut it down
 docker-compose -f "$<OVENA_CONFIG>/docker-compose.yml" stop 2> /dev/null
 
 echo "Create directories"
-rm -vrf "$<OVENA_CONFIG>" 
-mkdir -vp "$<OVENA_CONFIG>/nginx-reverse-proxy/keys" /usr/local/bin || exit 1
+if [ -d "$<OVENA_CONFIG>" ]; then
+    echo " ** WARNING ** existing configuration found! Will overwrite."
+fi
+# rm -vrf "$<OVENA_CONFIG>" 
+mkdir -vp "$<OVENA_CONFIG>" /usr/local/bin || exit 1
 
 echo "Update and install packages"
 export DEBIAN_FRONTEND=noninteractive && apt update && apt-get -y upgrade && apt-get -y install docker.io docker-compose cifs-utils || exit 1
@@ -15,7 +23,10 @@ apt autoremove
 
 echo "Copy configuration files"
 cp -vR docker/* "$<OVENA_CONFIG>" || exit 1
-cp -vR bin/ovena-* /usr/local/bin || exit 1
 chmod 600 "$<OVENA_CONFIG>/docker-compose.yml" || exit 1
+
+echo "Copy scripts in /usr/local/bin"
+rm -f /usr/local/bin/ovena-*
+cp -vR bin/ovena-* /usr/local/bin || exit 1
 
 cd "$<OVENA_CONFIG>" && docker-compose build && docker-compose up -d "$<DATABASE_DOCKER_IMAGE>"
