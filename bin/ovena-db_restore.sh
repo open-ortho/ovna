e2ed04ccab156a6c563ec69a4bf5ce6ad02d00c6
Toni Magni <amagni@ortocomputer.com>
2022-11-03 03:51:11 +0100

    Redid restore based on dropping volume.

--- bin/ovena-db_restore.sh
+++ bin/ovena-db_restore.sh
@@ -3,23 +3,29 @@
 # v0.2.0
 #
 # Restore orthanc PostgreSQL DB.
-# If no backup file is passed, will create the database user and empty database, used for initial DB setup.
 #
-
+# $DBUSER is the actual superuser of the database, as we assume this DB will only be uesd by orthanc.
+#
 BKPFILE="$1"
 DBNAME="$<DATABASE_NAME>"
 DBUSER="$<DATABASE_USERNAME>"
-alias _exec="docker-compose -f $<OVENA_CONFIG>/docker-compose.yml exec \"$<DATABASE_DOCKER_IMAGE>\""
-USEROPT="--username=postgres"
+alias _docker-compose="docker-compose -f $<OVENA_CONFIG>/docker-compose.yml"
+alias _exec="_docker-compose exec -T \"$<DATABASE_DOCKER_IMAGE>\""
+USEROPT="--username=$DBUSER"
 
-if [ -f "$BKPFILE" ]; then
-    _exec /usr/bin/dropdb $USEROPT "$DBNAME"
-    _exec /usr/bin/dropuser $USEROPT "$DBUSER"
+if [ ! -f "$BKPFILE" ]; then
+    echo "First and only argument must be gzipped SQL dump."
+    exit 1
 fi
 
-_exec /usr/bin/createuser $USEROPT "$DBUSER" || exit
-_exec /usr/bin/createdb $USEROPT --owner="$DBUSER" "$DBNAME" || exit
+echo "Dropping DB VOLUME"
+_docker-compose down
+docker volume rm "$<PROJECT_NAME>_pg_data" || exit
+
+_docker-compose up -d "$<DATABASE_DOCKER_IMAGE>" && sleep 5
+
+# DBUSER and DBNAME are created by default by the docker container
 
 if [ -f "$BKPFILE" ]; then
-    zcat "$1" | _exec /usr/bin/psql --username="$DBUSER"
+    zcat "$1" | _exec /usr/bin/psql -v ON_ERROR_STOP=1 $USEROPT && _docker-compose up -d && echo "Restore completed."
 fi
