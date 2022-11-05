188ad02b41f71330a0b00b40b0fa61deef25b127
Toni Magni <amagni@ortocomputer.com>
2022-11-05 13:08:46 +0100

    Fix issue with /tmp dir incorrect permissions

--- bin/ovena-install.sh
+++ bin/ovena-install.sh
@@ -11,18 +11,17 @@ This is Ovena Installer
 docker-compose -f "$<OVENA_CONFIG>/docker-compose.yml" stop 2>/dev/null
 
 echo "Create directories"
-if [ -d "$<OVENA_CONFIG>" ]; then
-    echo " ** WARNING ** existing configuration found! Will overwrite."
-fi
-# rm -vrf "$<OVENA_CONFIG>"
 mkdir -vp "$<OVENA_CONFIG>" /usr/local/bin || exit 1
 
 echo "Update and install packages"
+mkdir -p /tmp
+chown 0:0 /tmp
+chmod 1777 /tmp
 export DEBIAN_FRONTEND=noninteractive && apt update && apt-get -y upgrade && apt-get -y install docker.io docker-compose cifs-utils || exit 1
 apt autoremove
 
 echo "Copy configuration files"
-if [ ! -d "$<OVENA_CONFIG>/orthanc"]; then
+if [ ! -d "$<OVENA_CONFIG>/orthanc" ]; then
     cp -vR docker/* "$<OVENA_CONFIG>" || exit 1
 else
     echo "Found existing configuration. Will not overwrite with default config files."
@@ -30,7 +29,7 @@ fi
 chmod 600 "$<OVENA_CONFIG>/docker-compose.yml" || exit 1
 
 echo "Copy scripts in /usr/local/bin"
-rm -f /usr/local/bin/ovena-*
-cp -vR bin/ovena-* /usr/local/bin || exit 1
+rm -f /usr/local/bin/ovena*
+cp -vR bin/ovena* /usr/local/bin || exit 1
 
 cd "$<OVENA_CONFIG>" && docker-compose build && docker-compose up -d "$<DATABASE_DOCKER_IMAGE>"
