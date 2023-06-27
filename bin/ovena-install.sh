0e872004e8596f4486d5d229f8ca406868ef7846
Toni Magni <email@antoniomagni.com>
2023-06-27 21:42:31 +0200

    Add curl for upgrades

--- bin/ovena-install.sh
+++ bin/ovena-install.sh
@@ -17,7 +17,7 @@ echo "Update and install packages"
 mkdir -p /tmp
 chown 0:0 /tmp
 chmod 1777 /tmp
-export DEBIAN_FRONTEND=noninteractive && apt update && apt-get -y upgrade && apt-get -y install docker.io docker-compose cifs-utils || exit 1
+export DEBIAN_FRONTEND=noninteractive && apt update && apt-get -y upgrade && apt-get -y install docker.io docker-compose cifs-utils curl || exit 1
 apt autoremove
 
 echo "Copy configuration files"
