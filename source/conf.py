ab2e60cd70a30d75fdaa89882088f7cf639dfbfc
Toni Magni <email@antoniomagni.com>
2024-07-25 09:35:28 +0200

    Build bumped version.

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, open-ortho'
 author = 'open-ortho'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0'
+release = '0.4.1'
 
 
 # -- General configuration ---------------------------------------------------
